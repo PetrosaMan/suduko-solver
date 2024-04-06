697e505d569ed232ad50ee58f77404148c36173e
PetrosaMan <dw.todd.eng@gmail.com>
2024-04-06 09:23:11 +0200

    added stringToSudokuArray. Converts string to a 2d array

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,12 @@
+
+
 class SudokuSolver {
 
-  validate(puzzleString) {
+  validate(puzzleString) {   
+    const regex = /^[1-9.]{81}$/;
+    const validString = regex.test(puzzleString); 
+    console.log('validString: ',validString )   
+    return validString;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
@@ -15,10 +21,36 @@ class SudokuSolver {
 
   }
 
-  solve(puzzleString) {
-    
+  solve(puzzleString) {    
+    this.validate(puzzleString);
+    console.log("valid-result ", puzzleString)
   }
 }
 
 module.exports = SudokuSolver;
+
+//let textArea = document.getElementById("text-input");
+// import {puzzleAndSolutions } from './puzzle-strings';
+
+//document.addEventListener('DOMContentLoaded', function() {
+  // Load a simple puzzle into the text area
+//  textArea.value = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+//});
+
+//let textBox = document.querySelector('#text-input');
+
+//let textBoxChanged = function() {
+//  let textBoxValues = textBox.value.split('');
+//  console.log(textBoxValues);
+//}
+
+
+
+/**
+ * Export your functions for testing in Node.
+ * Note: The 'try' block is to prevent errors
+ * on the client side
+ */
+
+
 
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,31 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  function stringToSudokuArray(puzzleStr) {
+    // convert puzzle string to a two dimension array
+    if (puzzleStr.length !== 81) throw new Error('Invalid puzzle string length');    
+    let grid = [];
+    for (let i = 0; i < puzzleStr.length; i += 9) {
+      grid.push(puzzleStr.substring(i, i + 9).split(''));
+    }
+    return grid;
+  }
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
 
-    });
+  app.route("/api/check").post((req, res) => {
+    console.log("Check Placement");    
+    res.json({ checkPlacement: "checkPlacement" });
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    //log(req.body.puzzle);
+    let puzzleStr = req.body.puzzle;
+    const puzzleArr = stringToSudokuArray(puzzleStr)
+    console.log(puzzleArr);
+    //solver.solve(puzzleArr);
+  });
 };
